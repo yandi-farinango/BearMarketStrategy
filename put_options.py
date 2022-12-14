6be19ba36643fca827a3516eeb293f077c9a117c
Yandi Farinango <71475457+yandi-farinango@users.noreply.github.com>
2022-12-13 18:28:53 -0600

    Pulled more Put Data

--- put_options.py
+++ put_options.py
@@ -7,8 +7,28 @@ r.login(config.USERNAME, config.PASSWORD)
 
 symbol = ['AAPL']
 expirationDate = '2022-12-16'
+strike = 145
 
-optionData = r.options.find_options_by_expiration(inputSymbols=symbol, expirationDate=expirationDate, optionType='put')
 
-for item in optionData:
-    pprint(item)
+# # Option Data by Expiration
+# optionData = r.options.find_options_by_expiration(inputSymbols=symbol, expirationDate=expirationDate, optionType='put')
+
+# for option in optionData:
+#     if float(option['ask_price']) < 3.00:
+#         print('Strike Price: {}, Bid: {}, Ask: {}, High: {}, Low: {}'.format(
+#             option['strike_price'], option['bid_price'], option['ask_price'], option['high_price'], option['low_price']
+#         ))
+
+
+# # Option Data by Strike Price 
+# strike_145 = r.options.find_options_by_strike(symbol, strike, optionType='put')
+
+# strike_145 = sorted(strike_145, key=lambda x: x['expiration_date'])
+
+# for option in strike_145:
+#     print('Expiration Date: {}, Bid: {}, Ask: {}, High: {}, Low: {}'.format(
+#         option['expiration_date'], option['bid_price'], option['ask_price'], option['high_price'], option['low_price']
+#     ))
+
+put = r.options.get_option_instrument_data('AAPL', expirationDate, strike, optionType='put')
+pprint(put)
